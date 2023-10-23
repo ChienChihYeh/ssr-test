a56537c38c3255bc3ea69876725bc7ed7fcda443
ChienChihYeh <miroyeh1986@gmail.com>
2023-10-23 18:03:34 +0800

    fix style

--- app/(navigation)/list/list.tsx
+++ app/(navigation)/list/list.tsx
@@ -1,17 +1,17 @@
-"use client"
+"use client";
 
-import { useState } from "react"
+import { useState } from "react";
 
 export default function ListWrapper({
   itemArray,
   items,
 }: {
-  itemArray: React.ReactNode[]
-  items: string[]
+  itemArray: React.ReactNode[];
+  items: string[];
 }) {
   // export default function ListWrapper({ Item }: { Item: React.ReactElement }) {
-  const [itemIndex, setItemIndex] = useState(0)
-  const [isShowList, setIsShowList] = useState(false)
+  const [itemIndex, setItemIndex] = useState(0);
+  const [isShowList, setIsShowList] = useState(false);
 
   return (
     <div className="text-center ">
@@ -19,14 +19,14 @@ export default function ListWrapper({
       {isShowList && (
         <ol className="list-decimal w-max m-auto">
           {items.map((v, i) => {
-            return <li key={i}>{itemArray[i]}</li>
+            return <li key={i}>{itemArray[i]}</li>;
           })}
         </ol>
       )}
       <button
-        className="text-gray-200 bg-black p-1 pl-2 pr-2 rounded m-2 w-24"
+        className="text-secondary bg-primary p-1 pl-2 pr-2 rounded m-2 w-24"
         onClick={() => {
-          setIsShowList((prev) => !prev)
+          setIsShowList((prev) => !prev);
         }}
       >
         {isShowList ? "Hide" : "Show"} List
@@ -36,16 +36,16 @@ export default function ListWrapper({
       {items.map((v, i) => {
         return (
           <button
-            className="text-gray-200 bg-black p-1 pl-2 pr-2 rounded m-2 w-20"
+            className="text-secondary bg-primary p-1 pl-2 pr-2 rounded m-2 w-20"
             onClick={() => {
-              setItemIndex(i)
+              setItemIndex(i);
             }}
             key={i}
           >
             {v}
           </button>
-        )
+        );
       })}
     </div>
-  )
+  );
 }
--- app/(navigation)/pagination/loading.tsx
+++ app/(navigation)/pagination/loading.tsx
@@ -1,4 +1,8 @@
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
-  return <div className="w-full h-screen">Loading...</div>;
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      Loading...
+    </div>
+  );
 }
