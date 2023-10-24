ad4e0bc9f5e5cac2886097e5878f266c25f376b2
ChienChihYeh <miroyeh1986@gmail.com>
2023-10-24 13:23:54 +0800

    search in server component using server action

--- app/(navigation)/pagination/page.tsx
+++ app/(navigation)/pagination/page.tsx
@@ -1,5 +1,6 @@
-import Link from "next/link";
-import Search from "./search";
+import Link from "next/link"
+import Search from "./search"
+import { redirect } from "next/navigation"
 
 const fruitList = [
   "Apple",
@@ -22,75 +23,75 @@ const fruitList = [
   "Pomegranate",
   "Lemon",
   "Coconut",
-];
+]
 
 export default async function Pagination({
   searchParams,
 }: {
-  params: { slug: string };
-  searchParams: { [key: string]: string | string[] | undefined };
+  params: { slug: string }
+  searchParams: { [key: string]: string | string[] | undefined }
 }) {
-  let page = 1;
+  let page = 1
 
-  let isPageError = false;
+  let isPageError = false
 
   if (
     searchParams.page &&
     typeof searchParams.page === "string" &&
     parseInt(searchParams.page)
   ) {
-    page = parseInt(searchParams.page);
+    page = parseInt(searchParams.page)
   } else {
-    isPageError = true;
+    isPageError = true
   }
 
   const keyword =
     typeof searchParams.search === "string"
       ? searchParams.search.toLowerCase()
-      : "";
+      : ""
 
   const filteredFruitList = fruitList.filter((v) =>
     v.toLowerCase().includes(keyword)
-  );
-  const noFilteredResult = filteredFruitList.length === 0 && keyword;
+  )
+  const noFilteredResult = filteredFruitList.length === 0 && keyword
 
-  const itemPerPage = 3;
+  const itemPerPage = 3
   const totalPages = Math.max(
     Math.ceil(filteredFruitList.length / itemPerPage),
     1
-  );
+  )
   if (page > totalPages && !noFilteredResult) {
-    isPageError = true;
+    isPageError = true
   }
   const pages = (page: number, totalPages: number) => {
-    const pageRange = 2;
-    const pageArray: number[] = [];
-    const minBound = page - pageRange <= 0 ? -page + pageRange + 1 : 0;
+    const pageRange = 2
+    const pageArray: number[] = []
+    const minBound = page - pageRange <= 0 ? -page + pageRange + 1 : 0
     const maxBound =
-      page + pageRange > totalPages ? totalPages - page - pageRange : 0;
+      page + pageRange > totalPages ? totalPages - page - pageRange : 0
 
     for (let i = -pageRange + maxBound; i <= pageRange + minBound; i++) {
       if (page + i > 0 && page + i <= totalPages) {
-        pageArray.push(page + i);
+        pageArray.push(page + i)
       }
     }
-    return pageArray;
-  };
+    return pageArray
+  }
 
   const fruits = (page: number, filteredFruitList: string[]) => {
-    const startIndex = (page - 1) * itemPerPage;
+    const startIndex = (page - 1) * itemPerPage
     const fruitsInPage = filteredFruitList.slice(
       startIndex,
       startIndex + itemPerPage
-    );
-    return fruitsInPage;
-  };
+    )
+    return fruitsInPage
+  }
 
   function isKeyword(keyword: string): string {
     if (keyword) {
-      return "&search=" + keyword;
+      return "&search=" + keyword
     }
-    return "";
+    return ""
   }
 
   if (isPageError) {
@@ -105,16 +106,37 @@ export default async function Pagination({
         </Link>
         <Link href="/">Home</Link>
       </div>
-    );
+    )
+  }
+
+  async function getSearch(formData: FormData) {
+    "use server"
+    const searchKeyword = formData.get("keyword")?.toString().toLowerCase()
+
+    if (searchKeyword) {
+      redirect("/pagination?page=1&search=" + searchKeyword)
+    }
   }
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <h1 className="font-bold">Pagination</h1>
-      {/* <p>{JSON.stringify(searchParams)}</p> */}
-      <p>{totalPages}</p>
       <div className="m-1 text-center">
-        <Search />
+        {/* using server actions */}
+        <form action={getSearch}>
+          <input
+            name="keyword"
+            className="border border-black mr-1 w-1/2 outline-none text-black px-1"
+          />
+          <button
+            type="submit"
+            className="p-1 pl-2 pr-2 bg-primary text-secondary rounded m-1"
+          >
+            Search
+          </button>
+        </form>
+        {/* using useRouter in client */}
+        {/* <Search /> */}
         {keyword && (
           <p className="m-2">
             <span className="font-bold">Search:</span> {keyword}
@@ -133,7 +155,7 @@ export default async function Pagination({
         )}
         <ul className="list-disc w-fit mb-2 m-auto">
           {fruits(page, filteredFruitList).map((v, i) => {
-            return <li key={i}>{v}</li>;
+            return <li key={i}>{v}</li>
           })}
         </ul>
         {pages(page, totalPages).map((v, i) => {
@@ -145,7 +167,7 @@ export default async function Pagination({
               >
                 {v}
               </span>
-            );
+            )
 
           return (
             <Link
@@ -155,10 +177,10 @@ export default async function Pagination({
             >
               {v}
             </Link>
-          );
+          )
         })}
       </div>
       <Link href="/">Home</Link>
     </div>
-  );
+  )
 }
